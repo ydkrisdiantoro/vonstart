7a5ec8b5ee04696cd0a15c6b45c4737f729b2582
Yayan Dwi Krisdiantoro <y.d.krisdiantoro@gmail.com>
2024-12-19 22:19:24 +0700

    feat: melokalkan bootstrap

--- public/assets/js/vonstemp.js
+++ public/assets/js/vonstemp.js
@@ -1,15 +1,20 @@
-$(document).ready(function () {
-    $('#btnToggleSidebar').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $('#content').toggleClass('active');
-        $('#navbar').toggleClass('active');
-    });
-    $('#btnDismissSidebar').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $('#content').toggleClass('active');
-        $('#navbar').toggleClass('active');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    const btnToggleSidebar = document.getElementById('btnToggleSidebar');
+    const btnDismissSidebar = document.getElementById('btnDismissSidebar');
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+    const navbar = document.getElementById('navbar');
+
+    function toggleClasses() {
+        sidebar.classList.toggle('active');
+        content.classList.toggle('active');
+        navbar.classList.toggle('active');
+    }
+
+    btnToggleSidebar.addEventListener('click', toggleClasses);
+    btnDismissSidebar.addEventListener('click', toggleClasses);
 });
+
 
 function searchMenu() {
     var input, filter, ul, li, a, i;
--- resources/views/layouts/vcontrol_index.blade.php
+++ resources/views/layouts/vcontrol_index.blade.php
@@ -5,13 +5,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>
-
             @yield('title')
-
             {{ ' - '.config('vcontrol.app_name') }}
         </title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-icons.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/vonstemp.css') }}">
 
         <style>
@@ -21,8 +20,8 @@
                 opacity: .02;
             }
         </style>
-        @yield('extra-css')
 
+        @yield('extra-css')
     </head>
 
     <body>
@@ -30,12 +29,7 @@
 
         @yield('body')
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-            crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
-            integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
-            crossorigin="anonymous"></script>
+        <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('assets/js/vonstemp.js') }}"></script>
 
         @yield('extra-js')
